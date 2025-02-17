7623affa6ef880ec0db1924d64d452c4d314ff75
ROrodrigp <rodrigo.agp96@gmail.com>
2025-02-17 07:30:51 -0600

    Adding  src/tools_clean_data.py

--- src/tools_clean_data.py
+++ src/tools_clean_data.py
@@ -1,23 +1,22 @@
 def clean_data(df):
     """
     Realiza la limpieza de valores nulos y elimina columnas no deseadas.
-    
+
     :param df: DataFrame a limpiar.
     :return: DataFrame limpio.
     """
     df_cleaned = df.copy()
-    
+
     # Reemplazar nulos en LotFrontage con 0
     df_cleaned['LotFrontage'] = df_cleaned['LotFrontage'].fillna(0)
-    
+
     # Eliminar filas con valores nulos en MasVnrArea
     df_cleaned = df_cleaned.dropna(subset=['MasVnrArea'])
-    
+
     # Eliminar la columna GarageYrBlt
     df_cleaned = df_cleaned.drop(columns=['GarageYrBlt'])
-    
+
     # Eliminar cualquier otro nulo restante
     df_cleaned = df_cleaned.dropna()
-    
-    return df_cleaned
 
+    return df_cleaned
